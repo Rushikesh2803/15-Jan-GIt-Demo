7224756631c4ea9d40437f91ac85aaa526bcd99b
Dell <Dell@DESKTOP-A3QAI75>
2022-05-21 15:39:53 +0530

    m3 Method

--- src/test/DashBoard.java
+++ src/test/DashBoard.java
@@ -1,15 +1,23 @@
 package test;
 
-public class DashBoard {
-	public static void main(String[] args) {
+public class DashBoard 
+     {
+	public static void main(String[] args) 
+	{
 		System.out.println("This is DashBoardPage for push");
-		}
+	}
 	
 	public void m1() {
 		System.out.println("m1 Method");
 	}
 	
-	public void m2() {
+	public void m2() 
+	{
 		System.out.println("m2 Method");
+		
+	}
+	public void m3() 
+	{
+		System.out.println("m3 Method");
 	}
 }
