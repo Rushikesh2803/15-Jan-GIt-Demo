5c661533818580263a21db26df4852c59fde130b
Dell <Dell@DESKTOP-A3QAI75>
2022-05-21 15:56:40 +0530

    m1 removed from master

--- src/test/DashBoard.java
+++ src/test/DashBoard.java
@@ -1,26 +1,16 @@
 package test;
 
-public class DashBoard 
-     {
-	public static void main(String[] args) 
-	{
+public class DashBoard {
+	public static void main(String[] args) {
 		System.out.println("This is DashBoardPage for push");
-	}
+		}
 	
-	public void m1() {
-		System.out.println("m1 Method");
-	}
 	
-	public void m2() 
-	{
+	public void m2() {
 		System.out.println("m2 Method");
-		
 	}
-	public void m3() 
-	{
-		System.out.println("m3 Method");
-	}
-	public void m4() {
-		System.out.println("m4 Method");
+	
+	public void m6() {
+		System.out.println("m6 Method");
 	}
 }
